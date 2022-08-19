cce1eda8c94db9c8e64a6f952565517fa81dfcc5
Alexandre Umpierre <105897054+aumpierre-unb@users.noreply.github.com>
2022-08-19 03:19:59 -0300

    Add files via upload

---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+using InternalFluidFlow
+using Test
+using Plots
+
+@testset "InternalFluidFlow.jl" begin
+    @test abs(InternalFluidFlow.Re2f(1e3) - 64 / 1e3) < 1e-5
+    @test abs(InternalFluidFlow.Re2f(1e3, 1e-3, true) - 64 / 1e3) < 1e-5
+end
