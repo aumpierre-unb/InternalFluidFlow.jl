0bf5d7d79ff8233c89d21bab7b53e91de9509fe9
Alexandre Umpierre <105897054+aumpierre-unb@users.noreply.github.com>
2022-08-20 09:53:24 -0300

    Add files via upload

--- src/rough.jl
+++ src/rough.jl
@@ -11,7 +11,7 @@ function rough()
         eps = [eps; 10^w]
         f = [f; 1.01 * (2 * log10(3.7 / eps[end]))^-2]
         function foo(Re)
-            return 1 / sqrt(f[end]) + 2 * log10(eps / 3.7 + 2.51 / Re / sqrt(f))
+            return 1 / sqrt(f[end]) + 2 * log10(eps[end] / 3.7 + 2.51 / Re / sqrt(f[end]))
         end
         z = bissecao(foo, 1e3, 1e8, 1e-4)
         Re = [Re; z[end]]
