aab45710b50c5f96ad47799c9226e323faf0c10a
Alexandre Umpierre <105897054+aumpierre-unb@users.noreply.github.com>
2022-08-20 08:43:05 -0300

    Add files via upload

--- src/InternalFluidFlow.jl
+++ src/InternalFluidFlow.jl
@@ -3,6 +3,8 @@ module InternalFluidFlow
 using Plots
 using Test
 
+export Re2f, f2Re, hDeps2fRe, hveps2fRe, hQeps2fRe, hvthk2fRe, hQthk2fRe
+
 include("Re2f.jl")
 include("f2Re.jl")
 include("hDeps2fRe.jl")
