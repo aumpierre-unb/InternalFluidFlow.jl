25dd0aab6e1d68821609dfe41f3e59c64442500d
Alexandre Umpierre <105897054+aumpierre-unb@users.noreply.github.com>
2022-09-08 09:49:52 -0300

    Add files via upload

--- src/InternalFluidFlow.jl
+++ src/InternalFluidFlow.jl
@@ -1,3 +1,30 @@
+@doc raw"""
+
+This package provides a set of functions designed to solve problems 
+of internal fluid flow. All functions are based on the Poiseuille condition 
+for laminar flow, the Colebrook-White equation for turbulent flow, 
+and the Darcy-Weisbach equation for head loss. 
+The simplest problems on internal flow consist in computing 
+either the Reynolds number or the Darcy friction factor given 
+the other and the relative roughness. 
+For those cases, this package provides functions f2Re and Re2f, respectively. 
+More elaborated problems consist in computing both 
+the Reynolds number and the Darcy friction factor given the head loss, 
+the tube length, the fluid's density and dynamic viscosity, 
+the gravitational acceleration, the relative roughness and 
+either the dynamic diameter or the flow speed or the volumetric flow. 
+For those cases, this package provides functions 
+hDeps2fRe, hveps2fRe and hQeps2fRe, respectively. 
+A slightly more elaborate situation arises when 
+roughness is given instead of relative roughness along with 
+the flow speed or the volumetric flow. 
+For those cases, this package provides functions 
+hvthk2fRe and hQthk2fRe, respectively. 
+All function in this package offer the option of plotting the solution 
+on a schematic Moody diagram.
+
+See also: Re2f, f2Re, hDeps2fRe, hveps2fRe, hvthk2fRe, hQeps2fRe, hQthk2fRe
+"""
 module InternalFluidFlow
 
 using Plots
