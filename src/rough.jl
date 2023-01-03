157147da8bc9e82d48fce5053e6ef4ac0d1b7644
Alexandre Umpierre <105897054+aumpierre-unb@users.noreply.github.com>
2023-01-03 09:39:35 -0300

    Update rough.jl

--- src/rough.jl
+++ src/rough.jl
@@ -10,15 +10,12 @@ for a fully rough regime.
 the `InternalFluidFlow` toolbox for Julia.
 """
 function rough()
-    eps = []
-    f = []
-    Re = []
     N = 30
     u=log10(4e-5):(log10(5e-2) - log10(4e-5))/N:log10(5e-2)
     eps=10 .^ u
     f=1.01 .* (2 .* log10.(3.7 ./ eps)).^-2
+    Re=f2Re.(f,eps)
     display(plot!(Re, f,
         seriestype=:line,
         color=:blue))
 end
-
