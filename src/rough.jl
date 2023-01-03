73f32a7b1cc99fb14e1cdb2f722b8e00f040a2a0
Alexandre Umpierre <105897054+aumpierre-unb@users.noreply.github.com>
2023-01-03 10:24:21 -0300

    Update rough.jl

--- src/rough.jl
+++ src/rough.jl
@@ -14,7 +14,7 @@ function rough()
     u=log10(4e-5):(log10(5e-2) - log10(4e-5))/N:log10(5e-2)
     eps=10 .^ u
     f=1.01 .* (2 .* log10.(3.7 ./ eps)).^-2
-    Re=f2Re.(f,eps)[end]
+    Re=(f2Re.(f,eps))[end]
     # Re=[]
     # for n=1:length(f)
     #     re=f2Re(f[n],eps[n])
