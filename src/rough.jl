d1e0ee83ec9d8ddea5b61bcdf63cdf68110433db
aumpierre-unb <aumpierre@gmail.com>
2023-04-18 13:14:38 -0300

    Update rough.jl

--- src/rough.jl
+++ src/rough.jl
@@ -12,9 +12,9 @@ the `InternalFluidFlow` toolbox for Julia.
 function rough()
     N = 30
     u = log10(4e-5):(log10(5e-2)-log10(4e-5))/N:log10(5e-2)
-    eps = 10 .^ u
+    epssilon = 10 .^ u
     f = 1.01 .* (2 .* log10.(3.7 ./ eps)) .^ -2
-    Re = (f2Re.(f; isturb=true))
+    Re = f2Re.(f; eps=epsilon, isturb=false)
     plot!(Re, f,
         seriestype=:line,
         color=:blue)
