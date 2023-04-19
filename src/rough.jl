5ddfbd9103bec8facffeacc960a9c404664405e7
aumpierre-unb <aumpierre@gmail.com>
2023-04-19 13:28:09 -0300

    Update rough.jl

--- src/rough.jl
+++ src/rough.jl
@@ -22,7 +22,7 @@ function rough()
     for i = 1:N
         u = log10(5e-2) + (i - 1) * (log10(4e-5) - log10(5e-2)) / (N - 1)
         # z[i] = 10^u
-        # f = [f; 1.01 * (2 * log10(3.7 / z[end]))^-2]
+        # f = [f; 1.05 * (2 * log10(3.7 / z[end]))^-2]
         f[i] = 1.01 * (2 * log10(3.7 / 10^u))^-2
         w = f2Re(f[i], eps=10^u, isturb=false)
         Re[i] = w[end]
