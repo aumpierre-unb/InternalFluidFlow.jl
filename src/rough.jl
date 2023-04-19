05ede2bb159f9e14f1c628f6ffe17b423e95c462
aumpierre-unb <aumpierre@gmail.com>
2023-04-19 20:07:42 -0300

    Update rough.jl

--- src/rough.jl
+++ src/rough.jl
@@ -14,7 +14,7 @@ function rough()
     u = log10(4e-5):(log10(5e-2)-log10(4e-5))/N:log10(5e-2)
     z = 10 .^ u
     f = 1.01 .* (2 .* log10.(3.7 ./ z)) .^ -2
-    Re = f2Re.(f, eps=z; isturb=false)
+    Re = f2Re.(f, z; isturb=false)
     # N = 30
     # z = Vector{Float16}(undef, N)
     # f = Vector{Float16}(undef, N)
