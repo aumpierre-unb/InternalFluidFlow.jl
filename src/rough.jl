4fd75cfee20fea07616207201257305d5d6a2c20
aumpierre-unb <aumpierre@gmail.com>
2023-04-21 20:03:55 -0300

    Update rough.jl

--- src/rough.jl
+++ src/rough.jl
@@ -14,8 +14,8 @@ function rough()
     u = log10(4e-5):(log10(5e-2)-log10(4e-5))/N:log10(5e-2)
     z = 10 .^ u
     f = 1.01 .* (2 .* log10.(3.7 ./ z)) .^ -2
-    f2Re_(f,z)=f2Re(f,eps=z)
-    Re=f2Re_.(f, z, isturb=true)
+    f2Re_(f, z) = f2Re(f, eps=z, isturb=true)
+    Re = f2Re_.(f, z)
     plot!(Re, f,
         seriestype=:line,
         color=:blue)
