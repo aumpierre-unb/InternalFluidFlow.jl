1184301ca57f2c88c410b6b2052d601185d49557
aumpierre-unb <aumpierre@gmail.com>
2023-04-21 13:13:26 -0300

    Update InternalFluidFlow.jl

--- src/InternalFluidFlow.jl
+++ src/InternalFluidFlow.jl
@@ -17,10 +17,11 @@ module InternalFluidFlow
 using Plots
 using Test
 
-export Re2f, f2Re, hDeps2fRe, hveps2fRe, hQeps2fRe, hvthk2fRe, hQthk2fRe
+export Re2f, f2Re, h2fRe#, hDeps2fRe, hveps2fRe, hQeps2fRe, hvthk2fRe, hQthk2fRe
 
 include("Re2f.jl")
 include("f2Re.jl")
+include("h2fRe.jl")
 include("hDeps2fRe.jl")
 include("hveps2fRe.jl")
 include("hvthk2fRe.jl")
