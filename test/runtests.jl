84dc612ce275f537182170e4087f01dab6b989e3
Alexandre Umpierre <aumpierre@gmail.com>
2024-07-13 21:22:37 -0300

    update

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,10 @@ using Test
 using Plots
 
 @testset "InternalFluidFlow.jl" begin
-    @test abs(InternalFluidFlow.Re2f(1e3) - 64 / 1e3) < 1e-5
-    @test abs(InternalFluidFlow.Re2f(1e3, 1e-3, true) - 64 / 1e3) < 1e-5
+    @test abs(
+        InternalFluidFlow.Re2f(1e3) - 64 / 1e3
+    ) < 1e-5
+    @test abs(
+        InternalFluidFlow.Re2f(1e3, eps=1e-3, fig=true) - 64 / 1e3
+    ) < 1e-5
 end
