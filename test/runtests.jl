b32daaae0b7bdb084093a92d5ad0f9712d528d8a
Alexandre Umpierre <aumpierre@gmail.com>
2024-09-06 22:36:33 -0300

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,29 @@
 using InternalFluidFlow
 using Test
-using Plots
 
 @testset "InternalFluidFlow.jl" begin
+    @test all(
+        abs.(
+            InternalFluidFlow.h2fRe(
+                40,
+                D=10,
+                L=25e2,
+                ε=2.7e-3
+            ) .- (119032, 0.0265)
+        ) .< (1, 1e-4)
+    )
     @test abs(
-        InternalFluidFlow.Re2f(1e3) - 64 / 1e3
+        InternalFluidFlow.Re2f(
+            1e3,
+            ε=1e-3
+        ) - 64 / 1e3
     ) < 1e-5
-    @test abs(
-        InternalFluidFlow.Re2f(1e3, ε=1e-3, fig=true) - 64 / 1e3
-    ) < 1e-5
+    @test all(
+        abs.(
+            InternalFluidFlow.f2Re(
+                2.8e-2,
+                ε=1e-3
+            ) .- [2285, 19800]
+        ) .< [1, 1]
+    )
 end
