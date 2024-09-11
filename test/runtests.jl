02b5be4e368c7f1104bc8bd11c8bce67db410141
Alexandre Umpierre <aumpierre@gmail.com>
2024-09-11 07:25:40 -0300

    update

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using InternalFluidFlow
 using Test
 
 @testset "InternalFluidFlow.jl" begin
-    @test begin
+    @test "test h2fRe" begin
         moody = InternalFluidFlow.h2fRe(
             h=40,
             D=10,
@@ -13,7 +13,7 @@ using Test
         moody.Re - 119032 < 1 && moody.f - 0.0265 < 1e-4
     end
 
-    @test isnothing(
+    @test "test f2Re" isnothing(
         InternalFluidFlow.f2Re(
             f=1.3e-2,
             ε=5e-4,
@@ -21,7 +21,7 @@ using Test
         )
     )
 
-    @test begin
+    @test "test f2Re" begin
         moody = InternalFluidFlow.f2Re(
             f=2.8e-2,
             ε=1e-3,
@@ -30,7 +30,7 @@ using Test
         moody[1].Re - 2285 < 1 && moody[2].Re - 19800 < 1
     end
 
-    @test begin
+    @test "test Re2f" begin
         moody = InternalFluidFlow.Re2f(
             Re=1e3,
             ε=1e-3,
